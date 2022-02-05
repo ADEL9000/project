401ebafbdca8038f02464c9977cd49ecb0b7697a
ADEL9000 <adel.axmetov@mail.ru>
2022-02-05 22:26:04 +0500

    added product-details

--- app/js/main.js
+++ app/js/main.js
@@ -26,7 +26,7 @@ $(function(){
     "starSvg":'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/></svg>'
   });
 
-  $('.products__item-rating').rateYo({
+  $('.rating').rateYo({
     readOnly: true,
     spacing: "12px",
     starWidth: "18px",
@@ -34,6 +34,20 @@ $(function(){
     normalFill: "#d6d6d6",
     "starSvg":'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/></svg>'
   });
+
+  $('.slider-min').slick({
+    vertical: true,
+    slidesToShow: 3,
+    focusOnSelect: true,
+    asNavFor: '.slider-big',
+  });
+  $('.slider-big').slick({
+    fade: true,
+    arrows: false,
+    asNavFor: '.slider-min',
+  });
+
+  $('.details__form-input').styler()
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
   var containerEl2 = document.querySelector('[data-ref="container-2"]');
